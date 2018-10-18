a93fae16a96481d4e271dd1df8cfb439a1f61794
toptulla <ad.tsay@outlook.com>
2018-10-18 20:49:43 +0500

    Адаптив шапки страницы index

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let nav_toggle = document.getElementById('main-nav__toggle');
+    let nav_toggles = document.getElementsByClassName('nav-toggle');
+    let nav_toggle = nav_toggles[0];
+
     nav_toggle.onclick = function () {
-        let main_nav = document.getElementById('main-nav__list');
+        let nav__lists = document.getElementsByClassName('nav__list');
+        let site_nav = nav__lists[0];
+        let user_nav = nav__lists[1];
         
-        if(main_nav.style.display === 'block')
+        if(site_nav.style.display === 'block')
         {
-            main_nav.style.display = 'none';
-            nav_toggle.style.backgroundImage = "url('../img/menu.svg')";
+            site_nav.style = undefined;
+            user_nav.style = undefined;
+            nav_toggle.style = undefined;
         }
         else
         {
-            main_nav.style.display = 'block';
+            site_nav.style.display = 'block';
+            user_nav.style.display = 'block';
             nav_toggle.style.backgroundImage = "url('../img/menu-close.svg')";
         }
     };
 });
+
+function closeMobileNavigationOnMediaQuery(mediaQuery) {
+    if (mediaQuery.matches) {
+        let nav_toggles = document.getElementsByClassName('nav-toggle');
+        let nav_toggle = nav_toggles[0];
+
+        let nav__lists = document.getElementsByClassName('nav__list');
+        let site_nav = nav__lists[0];
+        let user_nav = nav__lists[1];
+
+        site_nav.style = undefined;
+        user_nav.style = undefined;
+        nav_toggle.style = undefined;
+    }
+};
+
+var x = window.matchMedia('(min-width: 768px)');
+x.addListener(closeMobileNavigationOnMediaQuery);
